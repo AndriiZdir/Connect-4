494911d076eddb999f86c37a80d666312796b962
McleanRozen <mclean.rozen@innovationcharter.org>
2015-10-21 10:28:11 -0400

    Fix filename

---
+++ Connect4.py
@@ -0,0 +1,42 @@
+def initializeGrid():
+
+def newPiece():
+def checkVictory():
+def checkFull():
+def buildGrid():
+def findVacancy():
+
+
+
+def test_initializeGrid():
+	grid = initializeGrid
+	print (grid)
+
+def test_getColumn():
+	column = getColumn
+	print (column)
+
+def test_newPiece():
+	newPiece(1)
+        print grid
+	newPiece(2)
+        print grid
+	newPiece(3)
+        print grid
+	newPiece(4)
+        print grid
+	newPiece(5)
+        print grid
+	newPiece(6)
+        print grid
+	newPiece(7)
+        print grid
+
+def test_checkVictory():
+grid=[[" "," "," "," "," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "],[" "," "," "," "," "," "]]
+row=checkVictory(grid)
+print row
+
+def test_checkFull():
+def test_buildGrid():
+def find_findVacancy():
